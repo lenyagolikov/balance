e685c7cdab04b6ad79eaef7bb23b048ff6899482
lenyagolikov <jayhosee@gmail.com>
2022-01-27 00:32:21 +0300

    Added ping db

--- app/api/handlers/ping.py
+++ app/api/handlers/ping.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from app.api import deps
 
 router = APIRouter()
 
@@ -6,3 +10,12 @@ router = APIRouter()
 @router.get("/app")
 def ping_app():
     return {"message": "ok"}
+
+
+@router.get("/db")
+def ping_db(db: Session = Depends(deps.get_db)):
+    try:
+        db.execute(text("SELECT 1"))
+    except Exception:
+        return {"message": "pg is down"}
+    return {"message": "ps is up"}
