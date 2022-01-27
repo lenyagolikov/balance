2083d0131cb7fc365dc18c04851ceda5cab886bb
lenyagolikov <jayhosee@gmail.com>
2022-01-27 16:15:25 +0300

    Changed config for Postgres

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_HOSTS: str = os.getenv("POSTGRES_HOSTS")
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_DB: str = os.getenv("POSTGRES_DB")
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
             scheme="postgresql+psycopg2",
             user=values.get("POSTGRES_USER"),
             password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER"),
+            host=values.get("POSTGRES_HOSTS"),
             path=f"/{values.get('POSTGRES_DB') or ''}",
         )
 
