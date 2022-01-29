994b36a77a76a6f60df6bd4c527f096fd0f3f1bc
lenyagolikov <jayhosee@gmail.com>
2022-01-29 20:39:17 +0300

    Added history of transactions for users

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from sqlalchemy.orm import Session
 
-from app.models import User
+from app.models import User, Transaction
 from app.schemas import UserCreate
 
 
@@ -11,21 +12,54 @@ def get(db: Session, user_id: int) -> User:
 
 def create(db: Session, request: UserCreate) -> User:
     user = User(id=request.id, balance=request.amount)
+    transaction = Transaction(
+        user_id=user,
+        type="deposit",
+        amount=request.amount,
+        at_created=datetime.utcnow(),
+    )
+    user.transactions.append(transaction)
+
     db.add(user)
     db.commit()
+
     return user
 
 
 def update(db: Session, user: User, amount: int) -> User:
+    if amount > 0:
+        transaction = Transaction(
+            user_id=user, type="deposit", amount=amount, at_created=datetime.utcnow()
+        )
+    else:
+        transaction = Transaction(
+            user_id=user, type="withdraw", amount=amount, at_created=datetime.utcnow()
+        )
+
     user.balance += amount
+    user.transactions.append(transaction)
+
     db.add(user)
     db.commit()
+
     return user
 
 
 def transfer(db: Session, sender: User, receiver: User, amount: int):
+    current_time = datetime.utcnow()
+
+    sender_transaction = Transaction(
+        user_id=sender, type="transfer", amount=-amount, at_created=current_time
+    )
+    receiver_transaction = Transaction(
+        user_id=receiver, type="transfer", amount=amount, at_created=current_time
+    )
+
     sender.balance -= amount
     receiver.balance += amount
+
+    sender.transactions.append(sender_transaction)
+    receiver.transactions.append(receiver_transaction)
 
     db.add_all([sender, receiver])
     db.commit()
