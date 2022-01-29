220913c1809f30069b890498cd941936984f5c36
lenyagolikov <jayhosee@gmail.com>
2022-01-29 18:25:32 +0300

    Changed init db for create tables in DB

--- app/init_db.py
+++ app/init_db.py
@@ -2,5 +2,8 @@ from app.db.base import Base
 from app.db.session import engine
 
 from app.models.user import User  # noqa
+from app.models.transaction import Transaction  # noqa
 
+
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
