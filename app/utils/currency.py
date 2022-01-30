98899f5a2bd931e7085a3def570ad9cfe5f9d8c7
lenyagolikov <jayhosee@gmail.com>
2022-01-30 15:29:42 +0300

    Change API for currency to USD, EUR

--- app/utils/currency.py
+++ app/utils/currency.py
@@ -1,29 +1,13 @@
 from decimal import Decimal
 
-import requests
+from forex_python.converter import CurrencyRates
 
-from app.core.config import settings
-
-
-URL = "http://api.exchangeratesapi.io/v1/"
-
-
-def get_unit_euro() -> dict[str, float]:
-    resp = requests.get(
-        f"{URL}latest?access_key={settings.EXCHANGE_RATES_API_TOKEN}&symbols=RUB"
-    )
-    return resp.json()["rates"]
-
-
-def convert_to_USD(amount: Decimal) -> Decimal:
-    resp = requests.get(
-        f"{URL}latest?access_key={settings.EXCHANGE_RATES_API_TOKEN}&symbols=USD"
-    )
-    unit_usd = Decimal(resp.json()["rates"]["USD"])
-    unit_euro = convert_to_EUR(amount)
-    return unit_usd * unit_euro
+api = CurrencyRates()
 
 
 def convert_to_EUR(amount: Decimal) -> Decimal:
-    unit_euro = Decimal(get_unit_euro()["RUB"])
-    return amount / unit_euro
+    return api.convert("RUB", "EUR", amount)
+
+
+def convert_to_USD(amount: Decimal) -> Decimal:
+    return api.convert("RUB", "USD", amount)
