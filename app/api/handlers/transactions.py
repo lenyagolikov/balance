d8d531c8e427b0cc8f0033cb6137fd3239511d13
lenyagolikov <jayhosee@gmail.com>
2022-01-30 16:14:13 +0300

    Move logic limit/offset from handler to crud

--- app/api/handlers/transactions.py
+++ app/api/handlers/transactions.py
@@ -12,9 +12,11 @@ router = APIRouter()
 def get_transactions(
     user_id: int, limit: int = 5, offset: int = 1, db: Session = Depends(deps.get_db)
 ):
-    transactions = crud.transaction.get(db=db, user_id=user_id)
+    transactions = crud.transaction.get(
+        db=db, user_id=user_id, limit=limit, offset=offset
+    )
     if not transactions:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="user not found"
         )
-    return transactions[offset - 1 : offset + limit - 1]
+    return transactions
--- app/crud/transaction.py
+++ app/crud/transaction.py
@@ -3,10 +3,11 @@ from sqlalchemy.orm import Session
 from app.models import Transaction
 
 
-def get(db: Session, user_id: int) -> list[Transaction]:
-    return (
+def get(db: Session, user_id: int, limit: int, offset: int) -> list[Transaction]:
+    transactions = (
         db.query(Transaction)
         .filter(Transaction.user_id == user_id)
         .order_by(Transaction.at_created.desc())
         .all()
     )
+    return transactions[offset - 1 : offset + limit - 1]
