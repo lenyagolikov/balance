bffb42461069df54af4f1414d7bb7ab0f6e5390f
lenyagolikov <jayhosee@gmail.com>
2022-01-30 14:38:32 +0300

    Implemented pagination and sorting by desc date

--- app/api/handlers/transactions.py
+++ app/api/handlers/transactions.py
@@ -9,10 +9,12 @@ router = APIRouter()
 
 
 @router.get("/{user_id}", response_model=list[Transaction])
-def get_transactions(user_id: int, db: Session = Depends(deps.get_db)):
+def get_transactions(
+    user_id: int, limit: int = 5, offset: int = 1, db: Session = Depends(deps.get_db)
+):
     transactions = crud.transaction.get(db=db, user_id=user_id)
     if not transactions:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="user not found"
         )
-    return transactions
+    return transactions[offset - 1 : offset + limit - 1]
--- app/crud/transaction.py
+++ app/crud/transaction.py
@@ -4,4 +4,9 @@ from app.models import Transaction
 
 
 def get(db: Session, user_id: int) -> list[Transaction]:
-    return db.query(Transaction).filter(Transaction.user_id == user_id).all()
+    return (
+        db.query(Transaction)
+        .filter(Transaction.user_id == user_id)
+        .order_by(Transaction.at_created.desc())
+        .all()
+    )
